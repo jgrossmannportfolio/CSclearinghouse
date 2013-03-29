b576b5b449cc4407bd831ece1ff57e7298321e5a
John Grossmann <JGrossmann@colgate.edu>
2013-03-29 00:37:52 -0400

    test code for show

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe ProjectsController do
-
+	describe 'going to a project details page' do
+		it 'should return the project details' do
+			fake_project = mock("project1")
+			Project.should_receive(:find).with("1").and_return(fake_project)
+			post :show, {:id => 1}
+		end
+	end
 end
