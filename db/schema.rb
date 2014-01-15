8710415de608f661cd6d342449256e395afdbd20
John Grossmann <JGrossmann@colgate.edu>
2014-01-14 20:18:41 -0500

    fixed jquery/javascript issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140111053104) do
+ActiveRecord::Schema.define(:version => 20140114235544) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -107,6 +107,8 @@ ActiveRecord::Schema.define(:version => 20140111053104) do
     t.text     "aboutme"
     t.string   "avatar"
     t.string   "resume"
+    t.text     "shortdescription"
+    t.string   "phone"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
